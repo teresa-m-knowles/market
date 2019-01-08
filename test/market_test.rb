f755c1ca163c78883038b05c65d2fbc68b90fa94
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-08 09:59:37 -0700

    writes tests for iteration 4

--- test/market_test.rb
+++ test/market_test.rb
@@ -173,5 +173,32 @@ class MarketTest < Minitest::Test
 
   end
 
+  def test_it_can_sell_an_item
+    market = Market.new("South Pearl Street Farmers Market")
+
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+
+    assert_equal false, market.sell("Peaches", 200)
+    assert_equal true, market.sell("Banana Nice Cream", 5)
+    assert_equal 45, vendor_2.check_stock("Banana Nice Cream")
+
+    assert_equal true, market.sell("Peaches", 40)
+    assert_equal 0, vendor_1.check_stock("Peaches")
+    assert_equal 60, vendor_3.check_stock("Peaches")
+  end
+
 
 end
